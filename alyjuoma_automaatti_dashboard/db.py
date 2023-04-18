17fd9d094d2fa2402c1c4be9b05f680628ed7c87
Ron Kosova <ron.kosova@gmail.com>
2023-04-18 20:08:15 +0300

    get_db function

---
+++ alyjuoma_automaatti_dashboard/db.py
@@ -0,0 +1,15 @@
+import psycopg2
+
+import click
+from flask import current_app, g
+
+def get_db():
+    if 'db' not in g:
+        g.db = psycopg2.connect(
+            host=current_app.config["DATABASE_HOST"],
+            database=current_app.config["DATABASE_NAME"],
+            user=current_app.config["DATABASE_USER"],
+            password=current_app.config["DATABASE_PASSWORD"]
+        )
+    
+    return g.db
