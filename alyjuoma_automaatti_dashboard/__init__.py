3297a3b847c2181435fea3e47541084671466042
Ron Kosova <ron.kosova@gmail.com>
2023-04-18 20:08:55 +0300

    Connected to configuration file in instance folder and test route

--- alyjuoma_automaatti_dashboard/__init__.py
+++ alyjuoma_automaatti_dashboard/__init__.py
@@ -4,10 +4,16 @@ from flask import Flask
 
 
 def create_app():
-    app = Flask(__name__)
+    app = Flask(__name__, instance_relative_config=True)
+
+    app.config.from_pyfile('config.py')
 
     @app.route("/hello")
     def hello():
         return 'Hello, World!'
+    
+    @app.route("/admin")
+    def admin():
+        return f"db: {app.config['DATABASE_HOST']} {app.config['DATABASE_NAME']} {app.config['DATABASE_USER']} {app.config['DATABASE_PASSWORD']}"
     
     return app
