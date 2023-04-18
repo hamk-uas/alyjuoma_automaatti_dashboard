54b606a6008f03f515bdb3dddeca94a0d3a2ca72
Ron Kosova <ron.kosova@gmail.com>
2023-04-18 22:13:31 +0300

    Changed database tech to MariaDB mySQL for dp.py

--- alyjuoma_automaatti_dashboard/db.py
+++ alyjuoma_automaatti_dashboard/db.py
@@ -1,10 +1,10 @@
-import psycopg2
+import mysql.connector
 
 from flask import current_app, g
 
 def get_db():
     if 'db' not in g:
-        g.db = psycopg2.connect(
+        g.db = mysql.connector.connect(
             host=current_app.config["DATABASE_HOST"],
             database=current_app.config["DATABASE_NAME"],
             user=current_app.config["DATABASE_USER"],
