135bd52134a4d7ff63bc6ed37716b11b3ba1b7fc
Ron Kosova <ron.kosova@gmail.com>
2023-05-01 21:41:07 +0300

    'download' blueprint: added view for downloading all data via '/download/all'

---
+++ alyjuoma_automaatti_dashboard/download.py
@@ -0,0 +1,28 @@
+import os
+from flask import (
+    Blueprint, request, make_response
+)
+
+from alyjuoma_automaatti_dashboard.data import (
+    data_all, data_slice
+)
+
+from alyjuoma_automaatti_dashboard.util import downloadable
+
+
+bp = Blueprint('download', __name__, url_prefix='/download')
+
+@bp.route('/all', methods=['GET'])
+def download_all():
+    data = data_all().get_json()
+    data = data["result"]
+
+    downloadable(data)
+    response = make_response(open('data.csv').read())
+    response.headers['Content-Disposition'] = 'attachment; filename=data.csv'
+    response.mimetype = 'text/csv'
+
+    os.remove('data.csv')
+
+    return response
+    
