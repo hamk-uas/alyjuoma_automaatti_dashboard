6260546129f5c29a1b73e3c1d5cfa7c00fdf7da2
Ron Kosova <ron.kosova@gmail.com>
2023-05-01 21:41:44 +0300

    added util module, downloadable function that turns data to downloadable temporary csv file

---
+++ alyjuoma_automaatti_dashboard/util.py
@@ -0,0 +1,7 @@
+import csv
+
+def downloadable(data):
+    csv_data = csv.writer(open('data.csv', 'w'))
+    csv_data.writerow(['id', 'dtime', 'farm_id', 'station_id', 'parameter_type', 'parameter_value'])
+    for row in data:
+        csv_data.writerow([row["id"], row["dtime"], row["farm_id"], row["station_id"], row["parameter_type"], row["parameter_value"]])
