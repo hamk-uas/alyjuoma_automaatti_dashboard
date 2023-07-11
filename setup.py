0968e81b768d81c1e84f5bff2c4f331d9f87220d
Ron Kosova <ron.kosova@gmail.com>
2023-07-11 21:22:02 +0300

    v0.1.1-alpha Fixed setup.py

--- setup.py
+++ setup.py
@@ -2,10 +2,12 @@ from setuptools import find_packages, setup
 
 setup(
     name='alyjuoma_automaatti_dashboard',
-    version='0.0.1',
+    version='v0.1.1-alpha',
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
         'flask',
+        'mysql-connector-python',
+        'freezegun'
     ],
 )
